8fd54e5b890ac66ce9b8897169f8fdd69bb299b4
Arash ghanbari <the.arash.ghanbari@gmail.com>
2025-07-24 16:35:25 +0330

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import "/styles/globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Ecomus",
@@ -8,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Header />
       <body>{children}</body>
+      <Footer />
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import Footer from "@/components/Footer";
+import { Header } from "@radix-ui/react-accordion";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
 }
