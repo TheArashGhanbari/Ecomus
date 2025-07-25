f24b4584d9e9034cffeb1cadd267e48f0964e2d4
Arash ghanbari <the.arash.ghanbari@gmail.com>
2025-07-25 17:07:38 +0330

    Added brand slider

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import BrandsSlider from "@/components/BrandSlider";
 import Footer from "@/components/Footer";
 import Map from "@/components/Map";
 import { Header } from "@radix-ui/react-accordion";
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <>
       <main className="min-h-screen grid grid-rows-[1fr_auto]">Home</main>
+      <BrandsSlider />
       <Map />
     </>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["ecomus-2-2.myshopify.com"],
+  },
+};
 
 export default nextConfig;
