2d7cc95d664bc12e07bcc41d42074f46ad087005
Arash ghanbari <the.arash.ghanbari@gmail.com>
2025-07-25 20:49:33 +0330

    update

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import BrandsSlider from "@/components/BrandSlider";
-import Footer from "@/components/Footer";
 import Map from "@/components/Map";
-import { Header } from "@radix-ui/react-accordion";
+import TestimonialSlider from "@/components/TestimonialSlider";
 
 export default function Home() {
   return (
     <>
       <main className="min-h-screen grid grid-rows-[1fr_auto]">Home</main>
+      <TestimonialSlider />
       <BrandsSlider />
       <Map />
     </>
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ecomus-2-2.myshopify.com"],
+    domains: [
+      "ecomus-2-2.myshopify.com",
+      "randomuser.me", // Add this for the new avatars
+    ],
   },
 };
 
